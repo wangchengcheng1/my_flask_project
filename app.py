4ff0b478053ca07c361230e32107e3431dd917f5
wangchengcheng <sc52292@163.com>
2024-12-08 23:40:41 +0900

    241208

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-HOSTNAME = '127.0.0.1'
+HOSTNAME = 'localhost'
 PORT = '3306'
-DATABASE = 'flask_demo'
+DATABASE = 'learn'
 USERNAME = 'root'
-PASSWORD = 'root'
+PASSWORD = '123456'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4'
 
 db=SQLAlchemy(app)
+
+
+with app.app_context():
+    try:
+        with db.engine.connect() as conn:
+            print("尝试连接数据库...")
+            rs = conn.execute(text('select 1'))
+            result = rs.fetchone()
+            print(f"数据库连接成功，结果：{result}")
+    except Exception as e:
+        print(f"数据库连接错误：{str(e)}")
+
 
 @app.route('/')
 def home():
@@ -22,4 +35,5 @@ def about(about_id):
     return f'About Flask! {about_id}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
